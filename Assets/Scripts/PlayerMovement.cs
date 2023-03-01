8ddd6c86a8f41b9f3197b8beb2c4f92b2a67c957
The Fire Owl <61467254+Owl-Man@users.noreply.github.com>
2023-03-01 14:16:37 +0300

    minor

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,15 +1,56 @@
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody))]
 public class PlayerMovement : MonoBehaviour
 {
-    [SerializeField] private Rigidbody rigidbody;
-    [SerializeField] private float speed;
+    public float Speed = 10f;
+    public float JumpForce = 300f;
+    private bool _isGrounded;
+    private Rigidbody _rb;
 
-    private void Update()
+    private void Start() => _rb = GetComponent<Rigidbody>();
+
+    private void FixedUpdate()
     {
-        float moveX = Input.GetAxis("Horizontal");
-        float moveY = Input.GetAxis("Vertical");
+        MovementLogic();
+        JumpLogic();
+    }
 
-        rigidbody.velocity = new Vector3(moveX * speed, rigidbody.velocity.y, moveY * speed);
+    private void MovementLogic()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        _rb.AddForce(movement * Speed);
+    }
+
+    private void JumpLogic()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            if (_isGrounded)
+            {
+                _rb.AddForce(Vector3.up * JumpForce);
+            }
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            _isGrounded = true;
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            _isGrounded = false;
+        }
     }
 }
